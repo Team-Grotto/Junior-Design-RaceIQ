aba8c159a2e9853c41626f8dd95930cc03f97cc5
Satvik Katla <katla.satvik@gmail.com>
2021-10-10 22:53:38 -0400

    Removed temporary form

--- frontend/src/Components/Configurations.js
+++ frontend/src/Components/Configurations.js
@@ -1,18 +1,14 @@
 import React, {Component} from 'react'
-i
+import Location from './Classes/Location';
+import Vehicle from './Classes/Vehicle';
+import Route from './Classes/Route';
 
 class Configurations extends Component {
     render() {
         return (
             <>
             <h1>This is the configuration page</h1>
-            <form>
-                <label>
-                    Name:
-                    <input type="text" name="name" />
-                </label>
-            <input type="submit" value="Submit" />
-            </form>
+            
             </>
         );
     }
