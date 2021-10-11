d6edc1558790de37e41af9a15e1ffdb06f428e95
Satvik Katla <katla.satvik@gmail.com>
2021-10-10 23:04:39 -0400

    Added buttons for adding vehicles and routes

--- frontend/src/Components/Configurations.js
+++ frontend/src/Components/Configurations.js
@@ -4,11 +4,16 @@ import Vehicle from './Classes/Vehicle';
 import Route from './Classes/Route';
 
 class Configurations extends Component {
+
+    
+
     render() {
         return (
             <>
             <h1>This is the configuration page</h1>
-            
+            <button onClick={() => this.addEmptyItem()}>Add Vehicle</button>
+            <button onClick={() => this.addEmptyItem()}>Add Route</button>
+            <button onClick={() => this.addEmptyItem()}>Pair Vehicle with route</button>
             </>
         );
     }
