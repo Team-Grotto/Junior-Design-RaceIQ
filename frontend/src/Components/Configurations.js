0dd46de1112d5739cf0419fc0fcf3647f1342340
Satvik Katla <katla.satvik@gmail.com>
2021-10-10 23:07:03 -0400

    Add methods to add the configurations

--- frontend/src/Components/Configurations.js
+++ frontend/src/Components/Configurations.js
@@ -4,15 +4,20 @@ import Vehicle from './Classes/Vehicle';
 import Route from './Classes/Route';
 
 class Configurations extends Component {
+    addVehicle() {
+        console.log("test vehicle");
+    }
+    addRoute() {
+        console.log("test route");
+    }
 
-    
 
     render() {
         return (
             <>
             <h1>This is the configuration page</h1>
-            <button onClick={() => this.addEmptyItem()}>Add Vehicle</button>
-            <button onClick={() => this.addEmptyItem()}>Add Route</button>
+            <button onClick={() => this.addVehicle()}>Add Vehicle</button>
+            <button onClick={() => this.addRoute()}>Add Route</button>
             <button onClick={() => this.addEmptyItem()}>Pair Vehicle with route</button>
             </>
         );
